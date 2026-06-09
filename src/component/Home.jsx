4d56ce3b99a5a688ad6f1d57f4bbeb6a7bc938ff
Keshav <keshav9899828464@gmail.com>
2026-06-09 21:13:03 +0530

    change in css

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -4,14 +4,14 @@ const Home = () => {
     return (<>
         <section>
             <div className="Image-Container">
-                <img src="https://i.pinimg.com/736x/59/fb/cc/59fbcc4a14f1fb10769bd27a1fdd9e12.jpg" alt="HomeShop" />
+                <img src="https://images.unsplash.com/photo-1441984904996-e0b6ba687e04?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="HomeShop" />
                 <NavLink to="/product"><CiShop /></NavLink>
             </div>
         </section>
         <section>
             <h3 className="category-head">Popular Categories</h3>
             <div className="categories-container">
-                
+
             </div>
         </section>
     </>
